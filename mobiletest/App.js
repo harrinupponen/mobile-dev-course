f326f28d8f4dfc54a371cff27ff47dd51e773214
Harri Nupponen <harrinupponen@gmail.com>
2020-03-06 15:15:59 +0200

    fix ex2, took away secret nr and guesses beside the advice text

--- mobiletest/App.js
+++ mobiletest/App.js
@@ -1,20 +1,65 @@
-import React from 'react';
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
-import Calculator from './components/Calculator';
-import History from './components/History';
-
-const AppNavigator = createStackNavigator(
-{
-  Calculator: {screen: Calculator},
-  History: {screen: History}
-}
-);
-
-const AppContainer = createAppContainer(AppNavigator);
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, Text, View, Button, Alert, TextInput} from 'react-native';
 
 export default function App() {
-  return (
-    <AppContainer />
-  );
+    const [secretNumber, setSecretNumber] = useState(0);
+    const [givenNumber, setGivenNumber] = useState('');
+    const [guesses, setGuesses] = useState(0);
+    const [advice, setAdvice] = useState('');
+
+    useEffect(() => {
+        start();
+    },[])
+
+    const  start = () => {
+        setSecretNumber(Math.floor(Math.random() * 100) +1)
+        setAdvice('Guess a number between 1-100')
+        setGuesses(0)
+    }
+
+    const check = () => {
+        setGuesses(guesses +1)
+        if(givenNumber < secretNumber) {
+            setAdvice('Your guess ' + givenNumber + ' is too low')
+        }
+        else if(givenNumber > secretNumber) {
+            setAdvice('Your guess ' + givenNumber + ' is too high')
+        }
+        else {
+            Alert.alert('You guessed the correct number in ' + (guesses + 1) + ' guesses')
+            start()
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text>{secretNumber} {advice} {guesses}</Text>
+            <TextInput 
+            keyboardType="numeric"
+            style={styles.tInput}
+            onChangeText={(givenNumber) => setGivenNumber(givenNumber)}
+            />
+            <View style={styles.buttons}>
+                <Button onPress={check} title="Make a guess" />
+            </View>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    tInput: {
+        borderColor: 'blue',
+        width: 100,
+        borderWidth: 2
+    },
+    buttons: {
+        flexDirection: 'row',
+        alignItems: 'flex-start'
+    }
+});
